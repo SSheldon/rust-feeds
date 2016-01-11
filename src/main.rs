ad24896cb8d78cc8236a06fac57bb51db1f03314
Steven Sheldon <steven@sasheldon.com>
2016-01-10 23:27:43 -0800

    Serve some dummy data.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 #[macro_use]
 extern crate iron;
+extern crate serde_json;
 
 mod api;
 
+use std::collections::BTreeMap;
 use std::io::Read;
 
 use iron::prelude::*;
 use iron::method;
 use iron::status;
+use serde_json::Value;
 
-use api::ApiRequest;
+use api::{ApiRequest, Feed, Item};
 
 fn handle_request(request: &mut Request) -> IronResult<Response> {
     match request.method {
@@ -25,7 +28,48 @@ fn handle_request(request: &mut Request) -> IronResult<Response> {
     itry!(request.body.read_to_string(&mut body));
     println!("{}", body);
 
-    Ok(Response::with((status::Ok, "{\"api_version\":1,\"auth\":1}")))
+    let feed = Feed {
+        id: 1,
+        title: "Hello feed".to_owned(),
+        url: "example.com".to_owned(),
+        last_updated_on_time: 1452495906,
+        ..Default::default()
+    };
+    let feed = feed.into_json();
+
+    let item = Item {
+        id: 1,
+        feed_id: 1,
+        title: "Hello item".to_owned(),
+        url: "example.com".to_owned(),
+        html: "Hello world!".to_owned(),
+        created_on_time: 1452495806,
+        ..Default::default()
+    };
+    let item = item.into_json();
+
+    let mut response = BTreeMap::new();
+    response.insert("api_version".to_owned(), Value::U64(1));
+    response.insert("auth".to_owned(), Value::U64(1));
+    match req_type {
+        ApiRequest::Feeds => {
+            response.insert("feeds".to_owned(), Value::Array(vec![feed]));
+            response.insert("feeds_groups".to_owned(), Value::Array(vec![]));
+        }
+        ApiRequest::LatestItems | ApiRequest::ItemsSince(_) => {
+            response.insert("items".to_owned(), Value::Array(vec![item]));
+            response.insert("total_items".to_owned(), Value::U64(1));
+        }
+        ApiRequest::UnreadItems => {
+            response.insert("unread_item_ids".to_owned(), Value::String("1".to_owned()));
+        }
+        _ => (),
+    }
+    let response = Value::Object(response);
+    let response = serde_json::to_string(&response).unwrap();
+    println!("{}", response);
+
+    Ok(Response::with((status::Ok, response)))
 }
 
 fn main() {
