364acb003cf08f566362c5c91ca92eae80cc3a44
Steven Sheldon <steven@sasheldon.com>
2017-03-12 11:19:53 -0700

    Added a title getter to Entry

--- feed-stream/src/entry.rs
+++ feed-stream/src/entry.rs
@@ -15,6 +15,15 @@ pub struct Entry {
     kind: EntryKind,
 }
 
+impl Entry {
+    pub fn title(&self) -> &str {
+        match self.kind {
+            EntryKind::Rss(ref item) => item.title.as_ref().map_or("", |s| &**s),
+            EntryKind::Atom(ref entry) => &entry.title,
+        }
+    }
+}
+
 pub fn from_xml(elem: Element) -> Option<Entry> {
     if elem.name.eq_ignore_ascii_case("item") {
         RssItem::from_xml(elem).ok().map(|item| {
