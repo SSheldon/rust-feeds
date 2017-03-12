97f3b8cb1a32b79b44a51ff4136d7332d7159c2f
Steven Sheldon <steven@sasheldon.com>
2017-03-12 11:07:52 -0700

    Added an entry::from_xml function

--- feed-stream/src/entry.rs
+++ feed-stream/src/entry.rs
@@ -1,5 +1,10 @@
+use std::ascii::AsciiExt;
+
 use atom_syndication::Entry as AtomEntry;
+use atom_syndication::FromXml;
 use rss::Item as RssItem;
+use rss::ViaXml;
+use xml::Element;
 
 enum EntryKind {
     Rss(RssItem),
@@ -8,4 +13,18 @@ enum EntryKind {
 
 pub struct Entry {
     kind: EntryKind,
+}
+
+pub fn from_xml(elem: Element) -> Option<Entry> {
+    if elem.name.eq_ignore_ascii_case("item") {
+        RssItem::from_xml(elem).ok().map(|item| {
+            Entry { kind: EntryKind::Rss(item) }
+        })
+    } else if elem.name.eq_ignore_ascii_case("entry") {
+        AtomEntry::from_xml(&elem).ok().map(|entry| {
+            Entry { kind: EntryKind::Atom(entry) }
+        })
+    } else {
+        None
+    }
 }
