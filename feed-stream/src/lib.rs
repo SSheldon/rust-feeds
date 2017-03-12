0d7c551e52883453ca61705420a6878f27ecd385
Steven Sheldon <steven@sasheldon.com>
2017-03-12 10:52:19 -0700

    Properly track state in the parser

--- feed-stream/src/lib.rs
+++ feed-stream/src/lib.rs
@@ -5,33 +5,71 @@ mod str_buf_reader;
 use std::ascii::AsciiExt;
 use std::io::Read;
 
-use xml::{Element, ElementBuilder, Event, Parser, ParserError, StartTag};
+use xml::{Element, ElementBuilder, EndTag, Event, Parser, ParserError, StartTag};
 
 use str_buf_reader::StrBufReader;
+
+#[derive(Clone, Copy)]
+enum ParserState {
+    None,
+    InRss,
+    InChannel,
+    InFeed,
+}
 
 pub struct RssParser<R> {
     reader: StrBufReader<R>,
     parser: Parser,
     builder: ElementBuilder,
+    state: ParserState,
 }
 
 impl<R: Read> RssParser<R> {
-    pub fn new(source: R) -> Result<RssParser<R>, ParserError> {
-        let mut parser = RssParser {
+    pub fn new(source: R) -> RssParser<R> {
+        RssParser {
             reader: StrBufReader::with_capacity(4096, source),
             parser: Parser::new(),
             builder: ElementBuilder::new(),
-        };
-
-        while let Some(event) = parser.next_event() {
-            let event = try!(event);
-            match event {
-                Event::ElementStart(StartTag { ref name, .. }) if name.eq_ignore_ascii_case("channel") => break,
-                _ => (),
-            }
+            state: ParserState::None,
         }
+    }
 
-        Ok(parser)
+    fn intercept_event(&mut self, event: &Event) -> bool {
+        match (self.state, event) {
+            (ParserState::None, &Event::ElementStart(StartTag { ref name, .. }))
+                    if name.eq_ignore_ascii_case("rss") => {
+                self.state = ParserState::InRss;
+                true
+            }
+            (ParserState::None, &Event::ElementStart(StartTag { ref name, .. }))
+                    if name.eq_ignore_ascii_case("feed") => {
+                self.state = ParserState::InFeed;
+                true
+            }
+            (ParserState::InRss, &Event::ElementStart(StartTag { ref name, .. }))
+                    if name.eq_ignore_ascii_case("channel") => {
+                self.state = ParserState::InChannel;
+                true
+            }
+            (ParserState::InRss, &Event::ElementEnd(EndTag { ref name, .. }))
+                    if name.eq_ignore_ascii_case("rss") => {
+                self.state = ParserState::None;
+                true
+            }
+            (ParserState::InChannel, &Event::ElementEnd(EndTag { ref name, .. }))
+                    if name.eq_ignore_ascii_case("channel") => {
+                self.state = ParserState::InRss;
+                true
+            }
+            (ParserState::InFeed, &Event::ElementEnd(EndTag { ref name, .. }))
+                    if name.eq_ignore_ascii_case("feed") => {
+                self.state = ParserState::None;
+                true
+            }
+            // Swallow all events until we're in a channel/feed
+            (ParserState::None, _) | (ParserState::InRss, _) => true,
+            _ => false
+        }
     }
 
     fn next_event(&mut self) -> Option<Result<Event, ParserError>> {
@@ -55,6 +93,11 @@ impl<R: Read> Iterator for RssParser<R> {
     fn next(&mut self) -> Option<Element> {
         while let Some(event) = self.next_event() {
             // println!("{:?}", event);
+
+            if let Ok(ref event) = event {
+                if self.intercept_event(event) { continue }
+            }
+
             match self.builder.handle_event(event) {
                 Some(Ok(elem)) => {
                     if elem.name.eq_ignore_ascii_case("item") {
@@ -92,7 +135,7 @@ mod tests {
 
     #[test]
     fn test_rss_stream() {
-        let mut parser = RssParser::new(RSS_STR.as_bytes()).unwrap();
+        let mut parser = RssParser::new(RSS_STR.as_bytes());
 
         let elem = parser.next().unwrap();
         assert_eq!(elem.name, "item");
