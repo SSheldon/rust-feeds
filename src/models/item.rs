e4061afe8a75f667e47c38bdefd5756416e10bac
Steven Sheldon <steven@sasheldon.com>
2017-03-27 10:07:46 -0700

    Added an into_api_item method for database Items

--- src/models/item.rs
+++ src/models/item.rs
@@ -1,5 +1,7 @@
 use chrono::NaiveDateTime;
 
+use fever_api::Item as ApiItem;
+
 #[derive(Queryable)]
 pub struct Item {
     pub id: i32,
@@ -7,4 +9,19 @@ pub struct Item {
     pub title: String,
     pub content: String,
     pub published: NaiveDateTime,
+}
+
+impl Item {
+    pub fn into_api_item(self, feed_id: u32) -> ApiItem {
+        ApiItem {
+            id: self.id as u32,
+            feed_id: feed_id,
+            title: self.title,
+            url: self.url,
+            html: self.content,
+            is_saved: false,
+            is_read: false,
+            created_on_time: self.published,
+        }
+    }
 }
