1cff4b075dcdfdd4d3d325601cc4b98b2620102c
Steven Sheldon <steven@sasheldon.com>
2018-09-20 13:04:09 -0700

    Implemented Display for ApiKey

--- fever-api/src/key.rs
+++ fever-api/src/key.rs
@@ -13,6 +13,9 @@ impl FromStr for ApiKey {
 
 impl Display for ApiKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for byte in self.0.iter() {
+            fmt::LowerHex::fmt(byte, f)?;
+        }
         Ok(())
     }
 }
