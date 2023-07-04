fd5f246c8664faebe6cb9f10b965f564d9e2d171
Steven Sheldon <steven@sasheldon.com>
2023-07-03 23:19:50 -0600

    Start working towards using serde for Google Reader

--- src/greader/request.rs
+++ src/greader/request.rs
@@ -1,25 +1,100 @@
 use chrono::NaiveDateTime;
+use serde_derive::Deserialize;
 
+#[derive(Deserialize)]
 pub enum StreamRanking {
+    #[serde(rename = "n")]
     NewestFirst,
+    #[serde(rename = "o")]
     OldestFirst,
 }
 
+#[derive(Deserialize)]
 pub enum StreamState {
+    #[serde(rename = "com.google/read")]
     Read,
+    #[serde(rename = "com.google/kept-unread")]
     KeptUnread,
+    #[serde(rename = "com.google/reading-list")]
     ReadingList,
+    #[serde(rename = "com.google/starred")]
     Starred,
 }
 
+#[derive(Deserialize)]
 pub enum StreamTag {
     Label(Option<String>, String),
     State(Option<String>, StreamState),
 }
 
+#[derive(Deserialize)]
 pub enum StreamId {
     Feed(String),
     Tag(StreamTag),
+}
+
+#[derive(Deserialize)]
+pub struct StreamContentsParams {
+    #[serde(rename = "r")]
+    ranking: StreamRanking,
+    #[serde(rename = "n")]
+    number: u32,
+    #[serde(rename = "c")]
+    continuation: String,
+    #[serde(rename = "xt")]
+    exclude: Option<StreamTag>,
+    #[serde(rename = "ot")]
+    exclude_older_than: Option<NaiveDateTime>,
+    #[serde(rename = "nt")]
+    exclude_newer_than: Option<NaiveDateTime>,
+}
+
+#[derive(Deserialize)]
+pub struct StreamItemsIdsParams {
+    #[serde(rename = "s")]
+    stream_id: StreamId,
+    #[serde(rename = "r")]
+    ranking: StreamRanking,
+    #[serde(rename = "n")]
+    number: u32,
+    #[serde(rename = "c")]
+    continuation: String,
+    #[serde(rename = "xt")]
+    exclude: Option<StreamTag>,
+    #[serde(rename = "ot")]
+    exclude_older_than: Option<NaiveDateTime>,
+    #[serde(rename = "nt")]
+    exclude_newer_than: Option<NaiveDateTime>,
+}
+
+#[derive(Deserialize)]
+pub struct StreamItemsCountParams {
+    #[serde(rename = "s")]
+    stream_id: StreamId,
+}
+
+#[derive(Deserialize)]
+pub struct StreamItemsContentsParams {
+    #[serde(rename = "i")]
+    item_ids: Vec<String>,
+}
+
+#[derive(Deserialize)]
+pub struct EditTagParams {
+    #[serde(rename = "i")]
+    item_ids: Vec<String>,
+    #[serde(rename = "a")]
+    add_tags: Vec<StreamTag>,
+    #[serde(rename = "r")]
+    remove_tags: Vec<StreamTag>,
+}
+
+#[derive(Deserialize)]
+pub struct MarkAllAsReadParams {
+    #[serde(rename = "s")]
+    stream_id: StreamId,
+    #[serde(rename = "ts")]
+    older_than: Option<NaiveDateTime>,
 }
 
 pub enum RequestType {
@@ -27,34 +102,11 @@ pub enum RequestType {
     UserInfo,
     UnreadCount,
     SubscriptionList,
-    StreamContents {
-        stream_id: StreamId,
-        ranking: StreamRanking,
-        number: u32,
-        continuation: String,
-        exclude: Option<StreamTag>,
-        exclude_older_than: Option<NaiveDateTime>,
-        exclude_newer_than: Option<NaiveDateTime>,
-    },
-    StreamItemsIds {
-        stream_id: StreamId,
-        ranking: StreamRanking,
-        number: u32,
-        continuation: String,
-        exclude: Option<StreamTag>,
-        exclude_older_than: Option<NaiveDateTime>,
-        exclude_newer_than: Option<NaiveDateTime>,
-    },
-    StreamItemsCount(StreamId),
-    StreamItemsContents(Vec<String>),
+    StreamContents(StreamId, StreamContentsParams),
+    StreamItemsIds(StreamItemsIdsParams),
+    StreamItemsCount(StreamItemsCountParams),
+    StreamItemsContents(StreamItemsContentsParams),
     TagList,
-    EditTag {
-        item_ids: Vec<String>,
-        add_tags: Vec<StreamTag>,
-        remove_tags: Vec<StreamTag>,
-    },
-    MarkAllAsRead {
-        stream_id: StreamId,
-        older_than: Option<NaiveDateTime>,
-    },
+    EditTag(EditTagParams),
+    MarkAllAsRead(MarkAllAsReadParams),
 }
