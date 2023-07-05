fd6536de32a6bc07a21f2e1ff3ea0cf32456e3e7
Steven Sheldon <steven@sasheldon.com>
2023-07-04 23:11:04 -0700

    Make pub

--- src/greader/response.rs
+++ src/greader/response.rs
@@ -5,127 +5,127 @@ use super::request::{StreamId, StreamTag};
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct UserInfoResponse {
-    user_id: String,
-    user_name: String,
-    user_profile_id: String,
-    user_email: String,
-    is_blogger_user: bool,
-    signup_time_sec: u64,
-    public_user_name: String,
+    pub user_id: String,
+    pub user_name: String,
+    pub user_profile_id: String,
+    pub user_email: String,
+    pub is_blogger_user: bool,
+    pub signup_time_sec: u64,
+    pub public_user_name: String,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct UnreadCountResponse {
-    max: u32,
-    unreadcounts: Vec<UnreadCount>,
+    pub max: u32,
+    pub unreadcounts: Vec<UnreadCount>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct UnreadCount {
-    count: u32,
-    id: StreamId,
-    newest_item_timestamp_usec: String,
+    pub count: u32,
+    pub id: StreamId,
+    pub newest_item_timestamp_usec: String,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct SubscriptionListResponse {
-    subscriptions: Vec<Subscription>,
+    pub subscriptions: Vec<Subscription>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct Subscription {
-    title: String,
-    first_item_msec: String,
-    html_url: String,
-    sort_id: String,
-    id: StreamId,
-    categories: Vec<SubscriptionCategories>,
+    pub title: String,
+    pub first_item_msec: String,
+    pub html_url: String,
+    pub sort_id: String,
+    pub id: StreamId,
+    pub categories: Vec<SubscriptionCategories>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct SubscriptionCategories {
-    id: StreamTag,
-    label: String,
+    pub id: StreamTag,
+    pub label: String,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct StreamContentsResponse {
-    direction: String,
-    author: String,
-    title: String,
-    updated: u64,
-    continuation: String,
-    id: StreamId,
-    self_links: Vec<Link>,
-    items: Vec<Item>,
+    pub direction: String,
+    pub author: String,
+    pub title: String,
+    pub updated: u64,
+    pub continuation: String,
+    pub id: StreamId,
+    pub self_links: Vec<Link>,
+    pub items: Vec<Item>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct Link {
-    href: String,
-    link_type: Option<String>,
+    pub href: String,
+    pub link_type: Option<String>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct Item {
-    updated: u64,
-    id: String,
-    categories: Vec<StreamTag>,
-    author: String,
-    alternate: Vec<Link>,
-    timestamp_usec: String,
-    content: ItemContent,
-    crawl_time_msec: String,
-    published: u64,
-    title: String,
+    pub updated: u64,
+    pub id: String,
+    pub categories: Vec<StreamTag>,
+    pub author: String,
+    pub alternate: Vec<Link>,
+    pub timestamp_usec: String,
+    pub content: ItemContent,
+    pub crawl_time_msec: String,
+    pub published: u64,
+    pub title: String,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct ItemContent {
-    direction: String,
-    content: String,
+    pub direction: String,
+    pub content: String,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct StreamItemsIdsResponse {
-    item_refs: Vec<ItemRef>,
+    pub item_refs: Vec<ItemRef>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct ItemRef {
-    id: String,
-    timestamp_usec: String,
-    direct_stream_ids: Vec<StreamId>,
+    pub id: String,
+    pub timestamp_usec: String,
+    pub direct_stream_ids: Vec<StreamId>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct StreamItemsContentsResponse {
-    items: Vec<Item>,
+    pub items: Vec<Item>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct TagListResponse {
-    tags: Vec<Tag>,
+    pub tags: Vec<Tag>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[derive(Serialize)]
 pub struct Tag {
-    id: StreamTag,
-    sort_id: String,
+    pub id: StreamTag,
+    pub sort_id: String,
 }
 
 pub enum Response {
