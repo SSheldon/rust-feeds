fa9744403a8b21179bfc9ab8632561bd7c5821cf
Steven Sheldon <steven@sasheldon.com>
2023-08-09 23:50:06 -0700

    Only prune items that were published before the latest
    The new fetching logic from 27b0d2c has a complicated bug: if a new item
    appears partway down the feed (most frequently caused by an existing
    item having its url changed), it will be one of the more recently
    inserted items. This means that items closer to the start of the feed
    which were actually inserted longer ago than it and will be pruned
    first. Once those get pruned, we identify them as new items and we
    insert them again and they become our new most recent items. The cycle
    repeats from here.
    
    I think the best way to address this is to prune a bit more
    conservatively. Specifically, everything would be fine if we wait to
    prune the newer-published-but-older-inserted items until after the
    older-published-but-newer-inserted item is also pruned.
    
    This updates the prune query to do that! We will now not prune any items
    that are newer published than the items we're keeping. This may result
    in us keeping more than 10 read rows, but it seems worth it.
    
    (It does fail against a pathological case where the top item on the feed
    is always older than any other item on a feed, but... I'll deal with
    that when I encounter such a feed.)

--- src/prune.sql
+++ src/prune.sql
@@ -1,7 +1,22 @@
-DELETE FROM item WHERE item.is_read AND NOT item.is_saved AND NOT item.id IN (
-    SELECT unnest(latest_items.ids) FROM (
-        SELECT feed.id, ARRAY(
-            SELECT item.id FROM item WHERE item.feed_id=feed.id ORDER BY item.id DESC LIMIT 10
-        ) FROM feed GROUP BY feed.id
-    ) as latest_items(feed_id, ids)
-);
+WITH numbered_item_stats AS (
+    SELECT
+        id,
+        published,
+        feed_id,
+        row_number() OVER (PARTITION BY feed_id ORDER BY id DESC) AS feed_num
+    FROM item
+), feed_latest_stats AS (
+    SELECT
+        feed_id,
+        min(id) AS min_id,
+        min(published) AS min_published
+    FROM numbered_item_stats
+    WHERE feed_num <= 10
+    GROUP BY feed_id
+)
+DELETE FROM item
+WHERE
+    is_read AND
+    NOT is_saved AND
+    id < (SELECT min_id FROM feed_latest_stats WHERE feed_id=item.feed_id) AND
+    published < (SELECT min_published FROM feed_latest_stats where feed_id=item.feed_id);
